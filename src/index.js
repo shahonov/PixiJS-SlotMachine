ee5c0306afdaddb00ba64c3f3dc9c411f92cf3eb
Martin Shahonov <shahonov@gmail.com>
2021-09-23 07:22:36 +0300

    added asset and canvas wrapper, included package lock in gitignore

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import * as PIXI from 'pixi.js';
 
-const app = new PIXI.Application(800, 600, { backgroundColor: 0x000000 });
+const width = window.innerWidth / 1.5;
+const height = window.innerHeight / 1.5;
 
-document.body.appendChild(app.view);
+const app = new PIXI.Application(width, height, { backgroundColor: 0x000000 });
+
+document.getElementById('canvas-wrapper').appendChild(app.view);
